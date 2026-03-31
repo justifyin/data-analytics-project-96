f9cf85f31863c02ae4c551326cfe145eb0644424
justifyin <96999619+justifyin@users.noreply.github.com>
2026-03-31 14:58:27 +0300

    Update dashboard.sql

--- dashboard.sql
+++ dashboard.sql
@@ -2,3 +2,30 @@
 SELECT COUNT(DISTINCT visitor_id) AS unique_visitors_count
 FROM sessions;
 
+/* Каналы, приводящие пользователей по дням */
+SELECT
+    SOURCE,
+    COUNT(*),
+    DATE_TRUNC('day', visit_date) as visit_date
+FROM SESSIONS
+GROUP BY 1, 3
+ORDER BY visit_date;
+
+/* Каналы, приводящие пользователей по неделям */
+SELECT
+    SOURCE,
+    COUNT(*),
+    DATE_TRUNC('day', visit_date) as visit_week
+FROM SESSIONS
+GROUP BY 1, 3
+ORDER BY visit_week;
+
+/* Приходящие лиды по дням */
+SELECT
+    DATE_TRUNC('day', created_at),
+    COUNT(*)
+FROM leads
+GROUP BY 1
+ORDER BY 1;
+
+/* Конверсия из клика в лид и из лида в оплату */
